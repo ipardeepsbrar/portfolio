2ce049851b0619fe71d458037806a0c5a7a2e5f6
Pardeep On Windows <brar68099@gmail.com>
2022-12-09 22:17:51 -0500

    on 09 dec 2022

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,15 +1,6 @@
-import {
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  IconButton,
-  Typography,
-} from "@mui/material";
 import React from "react";
-import LaunchIcon from "@mui/icons-material/Launch";
-import GitHubIcon from "@mui/icons-material/GitHub";
+import { MdLaunch } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
 
 import classes from "./MyCard.module.css";
 
@@ -17,38 +8,26 @@ const MyCard = (props) => {
   const {img, title, description, liveLink, gitHubLink} = props.item;
   return (
     <div className={classes.myCard}>
-      <Card>
-        <CardMedia component="img" image={img} className={classes.images} alt=""/>
-        <CardHeader
-          sx={{
-            color: "rgb(80,144,149)",
-            padding: "12px 10px 10px 10px",
-            borderTop: "0.6px solid rgb(102,102,102)",
-          }}
-          title={title}
-        ></CardHeader>
-        <CardContent sx={{ padding: "10px", paddingBottom:'0px', marginBottom:'32px' }}>
-          <Typography
-            sx={{ height: "2px", padding: "0px", paddingBottom: "0px" }}
-            variant="body2"
-            color="text.secondary"
-          >
-            {description}
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing sx={{ padding: "24px 10px 12px 10px" }}>
-          <a href={liveLink} target='blank' rel='noreferrer'>
-            <IconButton>
-              <LaunchIcon />
-            </IconButton>
-          </a>
-          <a href={gitHubLink} target='blank' rel='noreferrer'> 
-            <IconButton>
-              <GitHubIcon />
-            </IconButton>
-          </a>
-        </CardActions>
-      </Card>
+
+      <div className={classes.imageDiv}>
+        <img src={img} alt="" className={classes.image} />
+      </div>
+
+      <div className={classes.aboutImage}>
+       <div className={classes.title}>
+        <h3>{title}</h3>
+      </div>
+
+      <div className={classes.description}>
+          <p>{description}</p>
+      </div>
+
+      <div className={classes.linkIcons}>
+        <a href={liveLink} target="_blank" rel="noreferrer"><MdLaunch/></a>
+        <a href={gitHubLink} target="_blank" rel="noreferrer"><FaGithub/></a>
+      </div> 
+      </div>
+      
     </div>
   );
 };
