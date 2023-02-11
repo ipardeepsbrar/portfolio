5c703d5529dd5641584b9b354c2d471c25652873
Pardeep on Macbook <brar68099@gmail.com>
2023-02-11 13:40:36 -0500

    added grocery cart live link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,17 +2,18 @@ import React from "react";
 import MyCard from "./MyCard";
 import classes from "./Projects.module.css";
 import pic1 from "../images/project1.png";
-import pic2 from '../images/gCart.png';
-import pic3 from '../images/project3.png';
-import workstar from '../images/workstar2.png'
+import pic2 from "../images/gCart.png";
+import pic3 from "../images/project3.png";
+import workstar from "../images/workstar2.png";
 
 const list = [
   {
     img: workstar,
-    title:'Workstar',
-    description:'It is a complete MERN project made completely by myself. The fontend is a SPA built with React and some other small packages. The backend is a RESTfull API built with NodeJs and Express with full authentication.',
-    liveLink:'https://workstar-bypardeep.web.app/',
-    gitHubLink:'https://github.com/ipardeepsbrar/workstar.git'
+    title: "Workstar",
+    description:
+      "It is a complete MERN project made completely by myself. The fontend is a SPA built with React and some other small packages. The backend is a RESTfull API built with NodeJs and Express with full authentication.",
+    liveLink: "https://workstar-bypardeep.web.app/",
+    gitHubLink: "https://github.com/ipardeepsbrar/workstar.git",
   },
   {
     img: pic1,
@@ -23,18 +24,20 @@ const list = [
     gitHubLink: "https://github.com/ipardeepsbrar/foodOrderApp.git",
   },
   {
-    img:pic2,
-    title:'Grocery Cart',
-    description:'It is a web based grocery cart or a ToDo list with some cool features as editing an item.',
-    liveLink:'#',
-    gitHubLink:'https://github.com/ipardeepsbrar/grocery-cart.git'
+    img: pic2,
+    title: "Grocery Cart",
+    description:
+      "It is a web based grocery cart or a ToDo list with some cool features as editing an item.",
+    liveLink: "https://grocery-cart-9202f.web.app/",
+    gitHubLink: "https://github.com/ipardeepsbrar/grocery-cart.git",
   },
   {
     img: pic3,
-    title:'Figma Design Project',
-    description:'It is a project I did while doing Meta Frontend Developer Course. In this project I designed a wireframe and made a prototype of that.',
-    liveLink:'#',
-    gitHubLink:'https://github.com/ipardeepsbrar/figmaProject.git'
+    title: "Figma Design Project",
+    description:
+      "It is a project I did while doing Meta Frontend Developer Course. In this project I designed a wireframe and made a prototype of that.",
+    liveLink: "#",
+    gitHubLink: "https://github.com/ipardeepsbrar/figmaProject.git",
   },
 ];
 
